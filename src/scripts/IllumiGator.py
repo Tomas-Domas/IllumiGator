681600ea9e44065fd14af26cea40551bf1803445
Amber Handal <amberhandal24@gmail.com>
2023-06-14 03:51:59 -0400

    add background to game state

--- src/scripts/IllumiGator.py
+++ src/scripts/IllumiGator.py
@@ -7,16 +7,20 @@ windowConstants = util.get_constants()
 class GameObject(arcade.Window):
     def __init__(self):
         super().__init__(windowConstants.get('WINDOW_WIDTH'), windowConstants.get('WINDOW_HEIGHT'), windowConstants.get('WINDOW_TITLE'))
+        self.background = None
         self.set_mouse_visible(False)
         arcade.set_background_color(arcade.color.SKY_BLUE)
         self.player_list = None
-    
+
     def setup(self):
         self.game_state = 'menu'
 
+        background_path = ':resources:../../../../../assets/background.jpg'
+        self.background = arcade.load_texture(background_path)
+
         # Boiler-plate sprite code
         self.player_list = arcade.SpriteList()
-        gator_path = 'assets/gator.png'
+        gator_path = ':resources:../../../../../assets/gator.png'
         self.gator_sprite = arcade.Sprite(gator_path, 1)
         self.gator_sprite.center_x = windowConstants.get('WINDOW_WIDTH') // 2
         self.gator_sprite.center_y = windowConstants.get('WINDOW_HEIGHT') // 2
@@ -24,9 +28,13 @@ class GameObject(arcade.Window):
 
     def on_draw(self):
         self.clear()
+
         if self.game_state == 'menu':
             draw_menu()
         elif self.game_state == 'game':
+            arcade.draw_lrwh_rectangle_textured(0, 0,
+                                                util.WINDOW_WIDTH, util.WINDOW_HEIGHT,
+                                                self.background)
             self.player_list.draw()
     
     def on_key_press(self, key, key_modifiers):
@@ -36,12 +44,10 @@ class GameObject(arcade.Window):
             if key == arcade.key.ESCAPE:
                 arcade.close_window()
 
-
 def main():
     window = GameObject()
     window.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()

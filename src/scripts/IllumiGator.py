0874573270277db6b0996c43bdca597312d2c850
Richard <goldpugz@gmail.com>
2023-06-14 14:52:11 +0900

    added game state

--- src/scripts/IllumiGator.py
+++ src/scripts/IllumiGator.py
@@ -11,11 +11,22 @@ class GameObject(arcade.Window):
         arcade.set_background_color(arcade.color.SKY_BLUE)
     
     def setup(self):
-        pass
+        self.game_state = 'menu'
 
     def on_draw(self):
         self.clear()
-        draw_menu()
+        if self.game_state == 'menu':
+            draw_menu()
+        elif self.game_state == 'game':
+            arcade.draw_text('IN THE GAME STATE', windowConstants.get('WINDOW_WIDTH') // 2, windowConstants.get('WINDOW_HEIGHT') // 2,
+                             arcade.color.WHITE, 48, anchor_x='center')
+    
+    def on_key_press(self, key, key_modifiers):
+        if self.game_state == 'menu':
+            if key == arcade.key.ENTER:
+                self.game_state = 'game'
+            if key == arcade.key.ESCAPE:
+                arcade.close_window()
 
 
 def main():

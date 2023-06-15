7b47ab3edadade7017f9edd354fdfc611e581034
Richard <goldpugz@gmail.com>
2023-06-15 13:03:32 +0900

    refactored constants, added paused game state

--- src/scripts/IllumiGator.py
+++ src/scripts/IllumiGator.py
@@ -1,14 +1,12 @@
 import arcade
-from menus import draw_menu
+from menus import draw_ingame_menu, draw_title_menu
 from util import util
 import Shapes
 import numpy
 
-windowConstants = util.get_constants()
-
 class GameObject(arcade.Window):
     def __init__(self):
-        super().__init__(windowConstants.get('WINDOW_WIDTH'), windowConstants.get('WINDOW_HEIGHT'), windowConstants.get('WINDOW_TITLE'))
+        super().__init__(util.WINDOW_WIDTH, util.WINDOW_HEIGHT, util.WINDOW_TITLE)
         self.background = None
         self.set_mouse_visible(False)
         arcade.set_background_color(arcade.color.SKY_BLUE)
@@ -16,16 +14,19 @@ class GameObject(arcade.Window):
     
     def setup(self):
         self.game_state = 'menu'
-        self.elem_list = [Shapes.Rectangle(numpy.array([2.5, windowConstants.get('WINDOW_HEIGHT') // 2]), numpy.array([5, windowConstants.get('WINDOW_HEIGHT')]))]
+        self.elem_list = [Shapes.Rectangle(numpy.array([2.5, util.WINDOW_HEIGHT // 2]), numpy.array([5, util.WINDOW_HEIGHT]))]
 
     def on_draw(self):
         self.clear()
+        print(self.game_state)
 
         if self.game_state == 'menu':
-            draw_menu()
-        elif self.game_state == 'game':
+            draw_title_menu()
+        elif self.game_state == 'game' or self.game_state == 'paused':
             for elem in self.elem_list:
                 elem.draw()
+            if self.game_state == 'paused':
+                draw_ingame_menu()
     
     def on_key_press(self, key, key_modifiers):
         if self.game_state == 'menu':
@@ -33,6 +34,14 @@ class GameObject(arcade.Window):
                 self.game_state = 'game'
             if key == arcade.key.ESCAPE:
                 arcade.close_window()
+
+        elif self.game_state == 'game':
+            if key == arcade.key.ESCAPE:
+                self.game_state = 'paused'
+
+        elif self.game_state == 'paused':
+            if key == arcade.key.ESCAPE:
+                self.game_state = 'game'
 
 def main():
     window = GameObject()

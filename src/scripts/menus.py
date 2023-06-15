d493224c349222a5aeba2c0fb6d3dfcc7b1a67a3
Richard <goldpugz@gmail.com>
2023-06-15 13:04:25 +0900

    removed constant dictionary, added in_game_menu WIP

--- src/scripts/menus.py
+++ src/scripts/menus.py
@@ -1,12 +1,15 @@
 import arcade
 from util import util
 
-windowConstants = util.get_constants()
+x_midpoint = util.WINDOW_WIDTH // 2
+y_midpoint = util.WINDOW_HEIGHT // 2
 
-title_x = windowConstants.get('WINDOW_WIDTH') // 2
-title_y = windowConstants.get('WINDOW_HEIGHT') // 2
+def draw_title_menu():
+    arcade.draw_text("IllumiGator", x_midpoint, y_midpoint, arcade.color.WHITE, 48, anchor_x="center")
+    arcade.draw_text("Press ENTER to start", x_midpoint, y_midpoint - 50, arcade.color.WHITE, 24, anchor_x="center")
+    arcade.draw_text("Press ESCAPE to quit", x_midpoint, y_midpoint - 100, arcade.color.WHITE, 24, anchor_x="center")
 
-def draw_menu():
-    arcade.draw_text("IllumiGator", title_x, title_y, arcade.color.WHITE, 48, anchor_x="center")
-    arcade.draw_text("Press ENTER to start", title_x, title_y - 50, arcade.color.WHITE, 24, anchor_x="center")
-    arcade.draw_text("Press ESCAPE to quit", title_x, title_y - 100, arcade.color.WHITE, 24, anchor_x="center")
+# TODO: FIX IN_GAME MENU
+def draw_ingame_menu():
+    arcade.draw_rectangle_filled(x_midpoint, y_midpoint, util.WINDOW_WIDTH // 3, util.WINDOW_HEIGHT // 2, arcade.color.BLACK)
+    arcade.draw_text('PAUSED', x_midpoint, y_midpoint + util.WINDOW_HEIGHT // 4, arcade.color.WHITE, 48, anchor_x='center')

e9fcbe421c526088b08e5e2661c05f84b0d4b1fb
Richard <goldpugz@gmail.com>
2023-07-01 18:58:46 +0900

    added documentation to line_line_intersection

--- illumiphysics/src/lib.rs
+++ illumiphysics/src/lib.rs
@@ -10,7 +10,10 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 }
 
 /// TODO: Implement line_line intersection in rust
-fn get_line_line_intersection(ray_x: SMatrix<[f32; 2], 2, 1>) -> SMatrix<f32, 2, 1> {
+/// Ray param is a nalgebra 2D vector containing two vectors of size 2.
+/// Each vector contains 2, 32 bit floating points
+/// Returns 1D vector with intersection point
+fn get_line_line_intersection(ray: SMatrix<[f32; 2], 2, 1>) -> SMatrix<f32, 2, 1> {
     return SMatrix;
 }
 

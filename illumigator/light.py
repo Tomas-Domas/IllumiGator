bf54938c77e4dae830c2d838544658df4f773726
Amber Handal <amberhandal24@gmail.com>
2023-07-18 01:33:55 +0900

    light rays can flicker and are thicker and more translucent

--- illumigator/light.py
+++ illumigator/light.py
@@ -1,5 +1,9 @@
+import math
+
 import arcade
 import numpy
+
+import time
 
 
 class LightRay:
@@ -9,6 +13,7 @@ class LightRay:
         self._end = numpy.zeros(2)
         self._child_ray: LightRay | None = None
         self._generation = generation
+        self._flicker = 20
 
     def _generate_child_ray(self, direction):
         if self._child_ray is None:
@@ -22,17 +27,22 @@ class LightRay:
             self._child_ray._direction = direction
 
     def draw(self):
+        alpha = int(60 + 63 * math.sin(2 * time.time()))
+        color = (255, 0, 0, alpha + self._flicker)
         arcade.draw_line(
             self._origin[0],
             self._origin[1],
             self._end[0],
             self._end[1],
-            arcade.color.WHITE,
+            color,
+            10
         )
         if self._child_ray is not None:
             self._child_ray.draw()
 
-def get_raycast_results(ray_x1, ray_y1, ray_x2, ray_y2, line_x1, line_y1, line_x2, line_y2) -> tuple[numpy.ndarray, numpy.ndarray]:  # distances, line indices
+
+def get_raycast_results(ray_x1, ray_y1, ray_x2, ray_y2, line_x1, line_y1, line_x2, line_y2) -> \
+        tuple[numpy.ndarray, numpy.ndarray]:  # distances, line indices
     # Don't @ me...    https://en.wikipedia.org/wiki/Line-line_intersection#Given_two_points_on_each_line_segment
     # print("INPUTS")
     # print(ray_x1, ray_y1, ray_x2, ray_y2)
@@ -58,3 +68,7 @@ def get_raycast_results(ray_x1, ray_y1, ray_x2, ray_y2, line_x1, line_y1, line_x
     min_indices = numpy.argmin(u, axis=0)
 
     return u.T[:, min_indices].diagonal(), min_indices
+
+
+def update(self):
+    self.flicker = 10 * math.sin(time.time())

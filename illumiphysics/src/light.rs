75ec15b665045b2daf6238632f619fbe7371520b
Richard <goldpugz@gmail.com>
2023-07-18 14:42:45 +0900

    removed rust

--- illumiphysics/src/light.rs
+++
@@ -1,9 +0,0 @@
-use nalgebra::{SMatrix, vector};
-
-pub struct LightRay {
-    origin: SMatrix<i32, 1, 2>,
-    direction: SMatrix<i32, 1, 2>
-}
-
-/// Add LightRay implementations
-impl LightRay {}
